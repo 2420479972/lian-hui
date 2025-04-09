17f674ac19a1d0a489799392ea701ff3de5a81a3
2420479972 <2420479972@qq.com>
2025-04-09 13:13:25 +0800

    最新提交

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,7 +1,5 @@
 <template>
-  <div :class="['grid   py-3  text-xs text-gray-400 font-medium',`grid-cols-${cols}`]">
-      <slot name="header"></slot>
-  </div>
+  <component :is="Header"></component>
   <div :class="['flex-1 w-full overflow-y-auto overflow-x-hidden',customClass]">
     <slot name="body"></slot>
   </div>
@@ -12,9 +10,15 @@ type Props = {
   cols:number,
   customClass:string
 }
-withDefaults(defineProps<Props>(),{
+const props = withDefaults(defineProps<Props>(),{
   cols:4,
   customClass:''
+})
+
+const Header = h('div',{
+  class:['grid   py-3  text-xs text-gray-400 font-medium',`grid-cols-${props.cols}`]
+},{
+  header: () => h('h1', '我是标题'),
 })
 
 </script>
